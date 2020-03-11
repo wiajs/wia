ed9c20d76400304f483ba9aa8dbcdbb8d611291c
sibyl <sibyl@wia.pub>
2020-03-11 01:08:42 -0400

    update wia

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,28 @@
+const loose = true;
+
 const presets = [
   [
-    '@babel/env',
+    '@babel/preset-env',
     {
-      targets: {node: 'current'},
+			loose,
+      targets: {
+				// node: 'current' // 按系统安装的node版本作为目标
+        browsers: [
+          '>0.5%',
+          'last 2 versions',
+          'Firefox ESR',
+          'not ie <= 8',
+          'not dead',
+        ], // defaults
+			},
       // useBuiltIns: 'usage'
     },
   ],
 ];
 
 const plugins = [
-  ['@babel/plugin-proposal-class-properties', {loose: true}],
-  ['@babel/plugin-proposal-private-methods', {loose: true}],
+  ['@babel/plugin-proposal-class-properties', {loose}],
+  ['@babel/plugin-proposal-private-methods', {loose}],
   ['@babel/plugin-proposal-function-bind'],
   ['@babel/plugin-proposal-nullish-coalescing-operator'],
   ['@babel/plugin-proposal-optional-chaining'],
